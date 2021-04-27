87ca87c2d4d5904b6d60ecba38710bcbcaaea321
mjbelanic <matthewbelanic@gmail.com>
2021-04-26 17:50:45 -0700

    PT-bug fixes

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -7,40 +7,47 @@ public class Pickup : MonoBehaviour
     public PickupType type;
     public int value;
     private AudioClip pickupAudioCLip;
+    bool isColliding;
 
     public void Awake()
     {
         pickupAudioCLip = GetComponent<AudioSource>().clip;
+        isColliding = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        switch (type)
+        if (collision.gameObject.name == "Player")
         {
-            case PickupType.HEALTH:
-                AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
-                FindObjectOfType<Player>().IncreaseHealth(value);
-                Destroy(gameObject);
-                break;
-            case PickupType.POWER:
-                AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
-                FindObjectOfType<Player>().IncreasePower(value);
-                Destroy(gameObject);
-                break;
-            case PickupType.PERSON:
-                AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
-                FindObjectOfType<Player>().IncreaseSavePeopleCount(value);
-                FindObjectOfType<GameManager>().CheckAllMinersSaved();
-                Destroy(gameObject);
-                break;
-            case PickupType.TIME:
-                AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
-                FindObjectOfType<Player>().IncreaseTime(value);
-                FindObjectOfType<GameManager>().CheckAllMinersSaved();
-                Destroy(gameObject);
-                break;
-            default:
-                break;
+            if (isColliding) return;
+            isColliding = true;
+            switch (type)
+            {
+                case PickupType.HEALTH:
+                    AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
+                    FindObjectOfType<Player>().IncreaseHealth(value);
+                    Destroy(gameObject);
+                    break;
+                case PickupType.POWER:
+                    AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
+                    FindObjectOfType<Player>().IncreasePower(value);
+                    Destroy(gameObject);
+                    break;
+                case PickupType.PERSON:
+                    AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
+                    FindObjectOfType<Player>().IncreaseSavePeopleCount(value);
+                    FindObjectOfType<GameManager>().CheckAllMinersSaved();
+                    Destroy(gameObject);
+                    break;
+                case PickupType.TIME:
+                    AudioSource.PlayClipAtPoint(pickupAudioCLip, Camera.main.transform.position);
+                    FindObjectOfType<Player>().IncreaseTime(value);
+                    Destroy(gameObject);
+                    break;
+                default:
+                    break;
+            }
+            isColliding = false;
         }
     }
 }
